3ec91c7cf110e1963f0979f289c1d63c95440309
carlalaya <carla.laya@gmail.com>
2024-05-06 13:36:06 -0300

    Arreglos en css

--- app.js
+++ app.js
@@ -1,27 +1,2 @@
 
-document.getElementById("barberia").addEventListener("click", function linkBarberia() {
-    window.location.href  = "https://carlalaya.github.io/Barberia/"
-});
-
-document.getElementById("numeroSecreto").addEventListener("click", function linkNumeroSecreto() {
-    window.location.href  = "https://carlalaya.github.io/Juego_adivina_el_numero_secreto/"
-});
-
-document.getElementById("encriptarTexto").addEventListener("click", function linkEncriptador() {
-    window.location.href  = "https://carlalaya.github.io/Challenge_one_Encriptar-Texto/"
-});
-
-document.getElementById("calculadora").addEventListener("click", function calcular() {
-    window.location.href  = "https://carlalaya.github.io/Calculadora/"
-});
-document.getElementById("Proyecto_nasa").addEventListener("click", function Proyecto_nasa() {
-    window.location.href  = "https://carlalaya.github.io/ProyectoNASA_API/"
-});
-
-document.getElementById("tienda_germina").addEventListener("click", function tienda_germina() {
-    window.location.href  = "https://carlalaya.github.io/Prueba_carrito-de-compras/"
-});
-
-
-
 
